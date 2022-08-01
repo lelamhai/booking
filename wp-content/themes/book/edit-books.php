8c15540585072145a68cfddb14e81a7b7b9504be
lelamhai <leelamhair@gmail.com>
2022-08-01 21:24:54 +0700

    create key

--- wp-content/themes/book/edit-books.php
+++ wp-content/themes/book/edit-books.php
@@ -3,9 +3,53 @@
 * Template Name: edit-books
 */
 ?>
-
 <?php
     get_header();
+    $businessName = "business-name";
+    $businessAddress = "business-address";
+    $businessPhoneNumber = "business-phone-number";
+    $businessGoogleMap = "business-google-map";
+    $businessGoogleReview = "business-google-review";
+    $businessFacebook = "business-facebook";
+    $businessInstagram = "business-intergram";
+    $businessYoutube = "business-youtube";
+    $businessOpenHours = "business-open-hours";
+
+    $businessBackgroundColorHeader = "business-backgroundcolor-header";
+    $businessTextColorHeader = "business-text-color-header";
+    $businessLogoHeader = "business-logo-header";
+    $businessAddMenuHeader = "business-addmenu-header";
+    $businessLinkMenuHeader = "business-linkmenu-header";
+    $businessYoutubeVideoBanner = "business-youtubevideo-banner";
+
+    $businessBackgroundColorBody = "business-backgroundcolor-body";
+    $businessBackgroundButtonBody = "business-backgroundbutton-body";
+    $businessTextColorBody = "business-text-color-body";
+
+    $businessTitleWelcome = "business-title-welcome-body";
+    $businessContentWelcome = "business-title-welcome-body";
+    $businessSlider1Welcome = "business-slider1-welcome-body";
+    $businessSlider2Welcome = "business-slider2-welcome-body";
+    $businessSlider3Welcome = "business-slider3-welcome-body";
+    $businessSlider4Welcome = "business-slider4-welcome-body";
+    $businessSlider5Welcome = "business-slider5-welcome-body";
+
+    $businessBackgroundColorReviews = "business-backgroundcolor-reviews-body";
+    $businessTextColorReviews = "business-textcolor-reviews";
+    $businessContent1Reviews = "business-content1-reviews";
+    $businessContent2Reviews = "business-content2-reviews";
+    $businessContent3Reviews = "business-content3-reviews";
+
+    $businessTitleGift = "business-title-gift";
+    $businessContentGift = "business-content-gift";
+    $businessImageGift = "business-image-gift";
+
+    $businessBackgroundColorFooter = "business-backgroundcolor-footer";
+    $businessTextColorFooter = "business-textcolor-footer";
+    $businessAboutUsFooter = "business-aboutus-footer";
+    
+    $termsId = 1;
+    $policyId = 1;
 ?>
 <style>
     header,
@@ -69,7 +113,7 @@
             <div id="exTab1" class="container-fs">
                 <ul  class="nav nav-pills" id="myTab">
                     <li class="active">
-                        <a  href="#1a" data-toggle="tab">BUSINESSS INFO</a>
+                        <a  href="#1a" data-toggle="tab">BUSINESS INFO</a>
                     </li>
                     <li>
                         <a href="#2a" data-toggle="tab">WEBSITE INFO</a>
