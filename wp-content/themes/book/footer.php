2b6b85dbc6f9277280ee6d13797f811b379067a2
Hoan <hoan@genvr.com>
2022-09-02 11:26:08 +0700

    update layout home page 8

--- wp-content/themes/book/footer.php
+++ wp-content/themes/book/footer.php
@@ -1,4 +1,4 @@
-<footer>
+<footer class="BackgroundColorHeader">
        <div class="wrap-footer">
            <div class="footer-row2">©All Rights Reserved. Powered by <a href="http://softkeymarketing.com/" style="color: #fff;">Softkeymarketing.com</a></div>
        </div>
